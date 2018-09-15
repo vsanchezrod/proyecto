376d4043c39e4a8c40531658d094e56da629ce95
vsanchezrod <v.sanchez.rod@gmail.com>
2018-09-15 17:46:23 +0200

    Modulo Core - Estructura visual básica de la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Módulos
+import { CoreModule } from './core/core.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Componentes
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { MenuComponent } from './menu/menu.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { PieComponent } from './pie/pie.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CabeceraComponent,
+    MenuComponent,
+    PrincipalComponent,
+    PieComponent
+  ]
+})
+export class CoreModule { }
