3d633a8a4e64d532ff11163b8ac2d6574cb592bb
vsanchezrod <v.sanchez.rod@gmail.com>
2018-09-16 18:08:42 +0200

    Modelo de datos - Salida, Viaje y Coordenada

---
+++ src/app/modelos/salida.model.ts
@@ -0,0 +1,23 @@
+import { Coordenada} from './coordenada.model';
+
+export class Salida {
+
+  nombre: string;
+  categorias: Array[string];
+  descripciom: string;
+  nivel: string;
+  distancia: number;
+  fechaInicio: Date;
+  fechaFin: Date;
+  imagen: string;
+  puntoEncuentro: Coordenada;
+
+  constructor() {
+    this.categorias = [];
+    this.puntoEncuentro = {
+      latitud: 0,
+      longitud: 0
+    };
+  }
+
+}
