671c9bda60a5ed6c8278ebda2f22a5faef484fc9
vsanchezrod <v.sanchez.rod@gmail.com>
2018-09-16 16:17:15 +0200

    Calendario con eventos

--- src/app/calendario/calendario.component.ts
+++ src/app/calendario/calendario.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarioComponent implements OnInit {
 
+  public events: any[];
+
   constructor() { }
 
   ngOnInit() {
+
+    this.events = [
+      {
+        'title': 'Ruta en bici a Marrupe',
+        'start': '2018-09-16'
+      },
+      {
+        'title': 'Ruta en bici a Sotillo de las Palomas',
+        'start': '2018-09-17'
+      },
+      {
+        'title': 'Ruta en bici a Cervera',
+        'start': '2018-09-18'
+      }
+    ];
   }
 
 }
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -7,35 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  public events: any[];
-
   constructor() { }
 
   ngOnInit() {
-    this.events = [
-      {
-        "title": "All Day Event",
-        "start": "2016-01-01"
-      },
-      {
-        "title": "Long Event",
-        "start": "2016-01-07",
-        "end": "2016-01-10"
-      },
-      {
-        "title": "Repeating Event",
-        "start": "2016-01-09T16:00:00"
-      },
-      {
-        "title": "Repeating Event",
-        "start": "2016-01-16T16:00:00"
-      },
-      {
-        "title": "Conference",
-        "start": "2016-01-11",
-        "end": "2016-01-13"
-      }
-    ];
   }
 
 }
