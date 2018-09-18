8a3cf33333cd38f2d7e968d4b9ee05c1bd9e6792
vsanchezrod <v.sanchez.rod@gmail.com>
2018-09-18 18:51:29 +0200

    Formulario Registro Aproximación por data. No validaciones

--- src/app/login/registro/registro.component.ts
+++ src/app/login/registro/registro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  formularioRegistro: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.formularioRegistro = new FormGroup( {
+      'nombre': new FormControl(),
+      'apellido': new FormControl(),
+      'email': new FormControl(),
+      'password': new FormControl(),
+      'fechaNacimiento': new FormControl(),
+      'sexo': new FormControl(),
+      'provincia': new FormControl(),
+      'avatar': new FormControl(),
+      'intereses': new FormArray([
+          new FormControl() ]),
+      'terminos': new FormControl()
+
+    });
   }
 
+  enviarDatos() {
+    console.log(this.formularioRegistro.value);
+  }
 }
