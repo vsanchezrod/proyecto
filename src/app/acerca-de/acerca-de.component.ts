de062e5127e32dfd368f4cb077e876b094d05caa
vsanchezrod <v.sanchez.rod@gmail.com>
2018-09-24 10:27:27 +0200

    Estructura visual básica del módulo Acerca-de

--- src/app/acerca-de/acerca-de.component.ts
+++ src/app/acerca-de/acerca-de.component.ts
@@ -9,7 +9,6 @@ export class AcercaDeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
--- src/app/acerca-de/acerca-de.module.ts
+++ src/app/acerca-de/acerca-de.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 // Componentes
 import { AcercaDeComponent } from './acerca-de.component';
 
+// Modulos para Componentes de PrimeNG
+import { CardModule } from 'primeng/card';
+import { RatingModule } from 'primeng/rating';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CardModule,
+    RatingModule,
+    FormsModule
   ],
   declarations: [
     AcercaDeComponent
